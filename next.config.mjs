47da79f8d9dc6349d6db0399b54f5418ae1515c5
Wren Dougherty <wrench@Wrens-MacBook-Air.local>
2025-12-22 18:52:52 -0800

    Redirect all traffic to Railway frontend

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        destination: 'https://gracious-love-production-ec22.up.railway.app/:path*',
+        permanent: false,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
